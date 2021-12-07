0e0c4f2f2cc95007472ffe60f249d55aad4c5f5b
tauhid-hasan-dev <64667932+Hasan-2020@users.noreply.github.com>
2021-12-07 11:02:43 +0300

    adding h1

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,7 @@ import "./projects.scss";
 export default function Projects() {
     return (
         <div className= "projects">
-            
+            <h1>I am man </h1>
         </div>
     )
 }
